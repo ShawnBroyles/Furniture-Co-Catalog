528d3508b7979f0c0871c4654c8b0e871e3df232
Shawn Broyles <sbroyles3@hawkmail.hccfl.edu>
2018-04-16 05:05:29 -0400

    Created documentation for the Item form

--- Furniture Co. Catalog/frmItem.vb
+++ Furniture Co. Catalog/frmItem.vb
@@ -1,87 +1,303 @@
-﻿' Developed by Shawn Broyles
-' The Item form displays information about a specific item in the
-' catalog with the option to add the item with a specified quantitiy
-' to the shopping cart.
+﻿'''-------------------------------------------------------------------------------------------------
+''' <summary>   The Item form. </summary>
+'''
+''' <remarks>   This form displays information about a specific product from the catalog and gives the option for adding it to the shopping cart. </remarks>
+'''-------------------------------------------------------------------------------------------------
 
 Public Class frmItem
 
+    ''' <summary>   The constant integer for the current form. </summary>
     Const _cintForm As Integer = Forms.ITEM
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Welcome ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Welcome form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuWelcome_Click(sender As Object, e As EventArgs) Handles mnuWelcome.Click
         Navigate(Forms.WELCOME, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Registration ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Registration form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuRegistration_Click(sender As Object, e As EventArgs) Handles mnuRegistration.Click
         Navigate(Forms.REGISTRATION, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Login ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Login form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuLogin_Click(sender As Object, e As EventArgs) Handles mnuLogin.Click
         Navigate(Forms.LOGIN, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Catalog ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Catalog form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuCatalog_Click(sender As Object, e As EventArgs) Handles mnuCatalog.Click
         Navigate(Forms.CATALOG, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Checkout ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Checkout form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuCheckout_Click(sender As Object, e As EventArgs) Handles mnuCheckout.Click
         Navigate(Forms.CHECKOUT, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Account ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Account form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuAccount_Click(sender As Object, e As EventArgs) Handles mnuAccount.Click
         Navigate(Forms.ACCOUNT, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Item ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Item form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuItem_Click(sender As Object, e As EventArgs) Handles mnuItem.Click
         Navigate(Forms.ITEM, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Payment ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Payment form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuPayment_Click(sender As Object, e As EventArgs) Handles mnuPayment.Click
         Navigate(Forms.PAYMENT, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Reload ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for reloading the currently displayed form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuReload_Click(sender As Object, e As EventArgs) Handles mnuReload.Click
         ReloadForm(Me)
         ResetForm()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The SignOut ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for signing the user out of his/her account. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuSignOut_Click(sender As Object, e As EventArgs) Handles mnuSignOut.Click
         SignOut()
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Exit ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for terminating the application. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
         ExitApplication()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The About ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for telling the user information about the currently displayed form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuAbout_Click(sender As Object, e As EventArgs) Handles mnuAbout.Click
         AboutApplication(_cintForm)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Print ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for creating a print preview popup of the current form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuPrint_Click(sender As Object, e As EventArgs) Handles mnuPrint.Click
         PrintForm(pfPrintForm)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The frmItem_Load subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine is used for loading the defaults of this form when it is loaded. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub frmItem_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadFormDefaults(Me)
         ResetForm()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The ResetForm subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine resets all of the data on the form. </remarks>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub ResetForm()
-        updAmountInCart.Maximum = _CurrentItem.Stock
-        updAmountInCart.Value = _CurrentItem.GetShoppingCartItem().Quantity
-        lblProductIDOutput.Text = _CurrentItem.ID.ToString()
-        lblNameOutput.Text = _CurrentItem.Name
-        lblPriceOutput.Text = _CurrentItem.Price.ToString("C2")
-        lblStockOutput.Text = _CurrentItem.Stock.ToString()
-        lblFeeOutput.Text = _CurrentItem.Fee.ToString("C2")
-        lblCategoryOutput.Text = _CurrentItem.Category
-        lblDescriptionOutput.Text = _CurrentItem.Description
+        updAmountInCart.Maximum = gitmCurrentItem.Stock
+        updAmountInCart.Value = gitmCurrentItem.GetShoppingCartItem().Quantity
+        lblProductIDOutput.Text = gitmCurrentItem.ID.ToString()
+        lblNameOutput.Text = gitmCurrentItem.Name
+        lblPriceOutput.Text = gitmCurrentItem.Price.ToString("C2")
+        lblStockOutput.Text = gitmCurrentItem.Stock.ToString()
+        lblFeeOutput.Text = gitmCurrentItem.Fee.ToString("C2")
+        lblCategoryOutput.Text = gitmCurrentItem.Category
+        lblDescriptionOutput.Text = gitmCurrentItem.Description
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Refresh button subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine resets all of the data on the form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
         ResetForm()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The AmountIntCart NumericUpDown subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine changes the quantity of an item in the shopping cart when the value of the NumericUpDown object changes. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub updAmountInCart_ValueChanged(sender As Object, e As EventArgs) Handles updAmountInCart.ValueChanged
-        _CurrentItem.GetShoppingCartItem().Quantity = updAmountInCart.Value
+        gitmCurrentItem.GetShoppingCartItem().Quantity = updAmountInCart.Value
     End Sub
 End Class
