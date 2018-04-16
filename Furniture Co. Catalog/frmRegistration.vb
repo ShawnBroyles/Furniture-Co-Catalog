3d4fdc6dae8fcc8f1e450b6224dca362e6492983
Shawn Broyles <sbroyles3@hawkmail.hccfl.edu>
2018-04-16 05:19:35 -0400

    Created documentation for the Registration form

--- Furniture Co. Catalog/frmRegistration.vb
+++ Furniture Co. Catalog/frmRegistration.vb
@@ -1,66 +1,265 @@
-﻿' Developed by Shawn Broyles
-' The Registration form is used by customers to create accounts.
+﻿'''-------------------------------------------------------------------------------------------------
+''' <summary>   The Registration form. </summary>
+'''
+''' <remarks>   This form is used by customers to create accounts. </remarks>
+'''-------------------------------------------------------------------------------------------------
 
 Public Class frmRegistration
+    ''' <summary>   The constant integer for the current form. </summary>
     Const _cintForm As Integer = Forms.REGISTRATION
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Welcome ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Welcome form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuWelcome_Click(sender As Object, e As EventArgs) Handles mnuWelcome.Click
         Navigate(Forms.WELCOME, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Registration ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Registration form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuRegistration_Click(sender As Object, e As EventArgs) Handles mnuRegistration.Click
         Navigate(Forms.REGISTRATION, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Login ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Login form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuLogin_Click(sender As Object, e As EventArgs) Handles mnuLogin.Click
         Navigate(Forms.LOGIN, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Catalog ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Catalog form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuCatalog_Click(sender As Object, e As EventArgs) Handles mnuCatalog.Click
         Navigate(Forms.CATALOG, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Checkout ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Checkout form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuCheckout_Click(sender As Object, e As EventArgs) Handles mnuCheckout.Click
         Navigate(Forms.CHECKOUT, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Account ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Account form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuAccount_Click(sender As Object, e As EventArgs) Handles mnuAccount.Click
         Navigate(Forms.ACCOUNT, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Item ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Item form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuItem_Click(sender As Object, e As EventArgs) Handles mnuItem.Click
         Navigate(Forms.ITEM, Me)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Payment ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for navigating the user to the Payment form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuPayment_Click(sender As Object, e As EventArgs) Handles mnuPayment.Click
         Navigate(Forms.PAYMENT, Me)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Reload ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for reloading the current form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuReload_Click(sender As Object, e As EventArgs) Handles mnuReload.Click
         ReloadForm(Me)
         ClearForm()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The SignOut ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for signing out of an account. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuSignOut_Click(sender As Object, e As EventArgs) Handles mnuSignOut.Click
         SignOut()
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Exit ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for terminating the application. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
         ExitApplication()
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The About ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for creating a popup that tells the user some information about the current form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub mnuAbout_Click(sender As Object, e As EventArgs) Handles mnuAbout.Click
         AboutApplication(_cintForm)
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Print ToolStripMenuItem on the MenuStrip. </summary>
+    '''
+    ''' <remarks>   This button is used for creating a print preview of the current form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub mnuPrint_Click(sender As Object, e As EventArgs) Handles mnuPrint.Click
         PrintForm(pfPrintForm)
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The frmRegistration_Load subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine is used for loading the defaults of the current form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub frmRegistration_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadFormDefaults(Me)
         txtUsername.Focus()
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Submit button subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine is used for creating a new account in the database. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
         Const cstrSuccessTitle As String = "Success"
@@ -128,7 +327,7 @@ Public Class frmRegistration
         If (strErrorMessage.Equals(cstrBaseErrorMessage)) Then
             Try
                 SQLCreateAccount(strUsername, strPassword, strFName, strLName, strEmail, strPhone, strAddress)
-                _CurrentUser.SignIn(SQLGetRecordID(DatabaseTables.ACCOUNT, cstrUsernameField, strUsername))
+                gusrCurrentUser.SignIn(SQLGetRecordID(DatabaseTables.ACCOUNT, cstrUsernameField, strUsername))
                 MsgBox(cstrSuccessMessage, , cstrSuccessTitle)
                 ClearForm()
                 Navigate(Forms.CATALOG, Me)
@@ -142,6 +341,12 @@ Public Class frmRegistration
         End If
     End Sub
 
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The ClearForm subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine is used for clearing the form. </remarks>
+    '''-------------------------------------------------------------------------------------------------
+
     Private Sub ClearForm()
         txtUsername.Clear()
         txtPassword.Clear()
@@ -153,6 +358,19 @@ Public Class frmRegistration
         txtAddress.Clear()
         txtUsername.Focus()
     End Sub
+
+    '''-------------------------------------------------------------------------------------------------
+    ''' <summary>   The Clear button subroutine. </summary>
+    '''
+    ''' <remarks>   This subroutine is used for clearing the form. </remarks>
+    '''
+    ''' <param name="sender">
+    ''' Source of the event.
+    ''' </param>
+    ''' <param name="e">
+    ''' Event information.
+    ''' </param>
+    '''-------------------------------------------------------------------------------------------------
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
         ' Clearing textboxes and setting the focus to txtUsername
